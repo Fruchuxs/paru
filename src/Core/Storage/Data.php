5d2d81a53e1b7b1a4f0dc5758a885fde5c9c0e66
fruchuxs <fruchuxs@gmail.com>
2019-10-28 22:36:03 +0100

    Validation of name and mime type added

--- src/Core/Storage/Data.php
+++ src/Core/Storage/Data.php
@@ -2,25 +2,42 @@
 
 namespace Paru\Core\Storage;
 
+use Exception;
+use Paru\Core\Mime\MimeTypeHelper;
+
 class Data {
+
+    use MimeTypeHelper;
 
     private $uniqueName;
     private $payload;
     private $mimeType;
     private $meta;
 
-    public function __construct(string $uniqueName, $payload, string $mimeType, array $meta = array()) {
+    public function __construct(?string $uniqueName = null, $payload = null, ?string $mimeType = null, array $meta = array()) {
+        if (!$this->isValidUniqueName($uniqueName)) {
+            throw new Exception('Given name is not valid.');
+        }
+
+        if (!$this->isValidMimeType($mimeType)) {
+            throw new Exception('Given mimetype is not valid.');
+        }
+
         $this->uniqueName = $uniqueName;
         $this->payload = $payload;
         $this->mimeType = $mimeType;
         $this->meta = $meta;
     }
 
+    private function isValidUniqueName(?string $uniqueName): bool {
+        return $uniqueName === null || \preg_match('/^[a-z0-9.\-_]+$/i', $uniqueName);
+    }
+
     /**
      * Unique name of the data.
-     * The name must be fit the following rule ^[a-z0-9.\-_}+$
+     * The name must be fit the following rule ^[a-z0-9.\-_]+$
      */
-    public function getUniqueName(): string {
+    public function getUniqueName(): ?string {
         return $this->uniqueName;
     }
 
@@ -28,7 +45,7 @@ class Data {
      * The mime type of the data. Like text/markdown.
      * Should not be null.
      */
-    public function getMimeType(): string {
+    public function getMimeType(): ?string {
         return $this->mimeType;
     }
 
@@ -36,7 +53,7 @@ class Data {
      * Additional Meta data.
      * The array can be empty.
      */
-    public function getMeta(): array {
+    public function getMeta(): ?array {
         return $this->meta;
     }
 
@@ -45,6 +62,26 @@ class Data {
      */
     public function getPayload() {
         return $this->payload;
+    }
+
+    public function setUniqueName($uniqueName): Data {
+        $this->uniqueName = $uniqueName;
+        return $this;
+    }
+
+    public function setPayload($payload): Data {
+        $this->payload = $payload;
+        return $this;
+    }
+
+    public function setMimeType($mimeType): Data {
+        $this->mimeType = $mimeType;
+        return $this;
+    }
+
+    public function setMeta($meta): Data {
+        $this->meta = $meta;
+        return $this;
     }
 
 }
